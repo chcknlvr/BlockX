f42b2f53c56ea0dd4f995a5e3c331ca83671da9c
chcknlvr <77564494+chcknlvr@users.noreply.github.com>
2021-03-29 16:24:32 -0400

    changed some code to kotlin

---
+++ src/main/java/chcknlvr/blockx/tungstenArmorMaterial.kt
@@ -0,0 +1,49 @@
+package chcknlvr.blockx
+
+import net.minecraft.item.ArmorMaterial
+import net.minecraft.entity.EquipmentSlot
+import chcknlvr.blockx.tungstenArmorMaterial
+import net.minecraft.sound.SoundEvent
+import net.minecraft.recipe.Ingredient
+import chcknlvr.blockx.Blockx
+import net.minecraft.sound.SoundEvents
+
+class tungstenArmorMaterial : ArmorMaterial {
+    override fun getDurability(slot: EquipmentSlot): Int {
+        return BASE_DURABILITY[slot.entitySlotId] * 100
+    }
+
+    override fun getProtectionAmount(slot: EquipmentSlot): Int {
+        return PROTECTION_VALUES[slot.entitySlotId]
+    }
+
+    override fun getEnchantability(): Int {
+        return 30
+    }
+
+    override fun getEquipSound(): SoundEvent {
+        return SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE
+    }
+
+    override fun getRepairIngredient(): Ingredient {
+        return Ingredient.ofItems(Blockx.TUNGSTEN_INGOT)
+    }
+
+    override fun getName(): String {
+        return "tungsten"
+    }
+
+    override fun getToughness(): Float {
+        return 5.0f
+    }
+
+    override fun getKnockbackResistance(): Float {
+        return 0.0f
+    } // In which A is helmet, B chestplate, C leggings and D boots.
+
+    // For reference, Leather uses {1, 2, 3, 1}, and Diamond/Netherite {3, 6, 8, 3}
+    companion object {
+        private val BASE_DURABILITY = intArrayOf(13, 15, 16, 11)
+        private val PROTECTION_VALUES = intArrayOf(4, 7, 9, 4)
+    }
+}
