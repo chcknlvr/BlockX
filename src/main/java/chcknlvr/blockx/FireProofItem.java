41b59c4d63410c63700d491904415fe06c658166
Thomas Carey <mrclifton@gmail.com>
2021-04-01 16:20:55 -0400

    Made Wolframite generate in smaller patches

--- src/main/java/chcknlvr/blockx/FireProofItem.java
+++
@@ -1,9 +0,0 @@
-package chcknlvr.blockx;
-
-import net.minecraft.item.Item;
-
-public class FireProofItem extends Item {
-    public FireProofItem(Settings settings) {
-        super(settings.fireproof());
-    }
-}
